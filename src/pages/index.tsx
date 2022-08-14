a9f71582f11caa28a824c474e58c18bfc165ff92
조민혁 <chohyuk0725@gmail.com>
2022-08-14 20:37:23 +0900

    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import Head from "next/head";
 import Image from "next/image";
 
 const Home: NextPage = () => {
-  return <div className=" text-lg text-purple-500 text-[40px]">ddd</div>;
+  return <div className=" text-lg text-purple-500 text-[40px]">테스트</div>;
 };
 
 export default Home;
